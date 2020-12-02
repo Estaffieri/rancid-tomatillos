a63b15bcf611d7875062ea41183d3550cbdb41a4
Estelle Staffieri <estellelareinne@gmail.com>
2020-12-01 18:10:16 -0700

    Add hover styling to movie posters and remove initial borders

--- src/Components/MoviePoster/MoviePoster.js
+++ src/Components/MoviePoster/MoviePoster.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './MoviePoster.css'
 
 const MoviePoster = ( {image, title, rating}) => {
-    return(
+    return (
       <section className="poster">
-        <img src={ image } className="poster-image" />
-        <h3>{ title }</h3>
-        <p>{ rating }</p>
+        <img src={image} className="poster-image" />
+        <h3>{title}</h3>
+        <p>&#11088; {rating}</p>
       </section>
-    )
+    );
 }
 
 export default MoviePoster;
