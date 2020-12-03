461b9117fc77a2ccd181737922f88fc68bc2ce39
Estelle Staffieri <estellelareinne@gmail.com>
2020-12-02 20:20:39 -0700

    Add movie info to movie detail view

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -3,19 +3,34 @@ import "./MovieDetails.css";
 import MovieContainer from '../MovieContainer/MovieContainer';
 
 const MovieDetails = ({ movieDetails }) => {
-    console.log(movieDetails)
    
-    const details = 
-   
-      <section
-        tabIndex={0}
-        className="poster"
-        // onClick={() => displayMovieDetails(id)}
-      >
-        <img src={ movieDetails[0].movie.poster_path } className="poster-image" alt={movieDetails[0].movie.title} />
-        <h3>{ movieDetails[0].movie.title }</h3>
-        <p>&#11088; { movieDetails[0].movie.average_rating }</p>
+    const details = (
+      <section>
+        <section
+          tabIndex={0}
+          className="poster"
+          // onClick={() => displayMovieDetails(id)}
+        >
+          <img
+            src={movieDetails[0].movie.poster_path}
+            className="poster-image"
+            alt={movieDetails[0].movie.title}
+          />
+        </section>
+
+        <section className="movie-info">
+          <h3>{movieDetails[0].movie.title}</h3>
+          <p>&#11088; {movieDetails[0].movie.average_rating}</p>
+          <p>{movieDetails[0].movie.runtime}</p>
+          <p>{movieDetails[0].movie.release_date}</p>
+          {/* <p>{movieDetails[0].movie.genres}</p> */}
+          <p>{movieDetails[0].movie.tagline}</p>
+          <p>{movieDetails[0].movie.overview}</p>
+          <p>{movieDetails[0].movie.budget}</p>
+          <p>{movieDetails[0].movie.revenue}</p>
+        </section>
       </section>
+    );
     
     
 
