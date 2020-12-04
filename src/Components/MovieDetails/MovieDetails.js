7e96c9a60c498cc496480309f8a5f8982a2b7cfa
Estelle Staffieri <estellelareinne@gmail.com>
2020-12-03 17:38:46 -0700

    Add styling to have movie poster and movie information side by side

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -6,28 +6,31 @@ const MovieDetails = ({ movieDetails }) => {
 
     const details = (
       <section>
-        <section className="back-button">
-          <button>Go Back</button>
-        </section>
+        <section className="macro-container">
+          <section tabIndex={0} className="poster">
+            <img
+              src={movieDetails[0].movie.poster_path}
+              className="poster-image"
+              alt={movieDetails[0].movie.title}
+            />
+          </section>
 
-        <section tabIndex={0} className="poster">
-          <img
-            src={movieDetails[0].movie.poster_path}
-            className="poster-image"
-            alt={movieDetails[0].movie.title}
-          />
-        </section>
+          <section className="movie-info">
 
-        <section className="movie-info">
-          <h3>{movieDetails[0].movie.title}</h3>
-          <p>&#11088; {movieDetails[0].movie.average_rating}</p>
-          <p>{movieDetails[0].movie.runtime}</p>
-          <p>{movieDetails[0].movie.release_date}</p>
-          <p>{movieDetails[0].movie.genres[0].name}</p>
-          <p>{movieDetails[0].movie.tagline}</p>
-          <p>{movieDetails[0].movie.overview}</p>
-          <p>{movieDetails[0].movie.budget}</p>
-          <p>{movieDetails[0].movie.revenue}</p>
+            <section className="back-button">
+              <button>Go Back</button>
+            </section>
+            
+            <h3>{movieDetails[0].movie.title}</h3>
+            <p>&#11088; {movieDetails[0].movie.average_rating}</p>
+            <p>{movieDetails[0].movie.runtime}</p>
+            <p>{movieDetails[0].movie.release_date}</p>
+            <p>{movieDetails[0].movie.genres[0].name}</p>
+            <p>{movieDetails[0].movie.tagline}</p>
+            <p>{movieDetails[0].movie.overview}</p>
+            <p>{movieDetails[0].movie.budget}</p>
+            <p>{movieDetails[0].movie.revenue}</p>
+          </section>
         </section>
       </section>
     );
