cd900c51fd76dba967223686ddcfa483bf6144f8
Estelle Staffieri <estellelareinne@gmail.com>
2020-12-03 19:20:05 -0700

    add sub sections for individual formatting in movie detail view

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -20,17 +20,24 @@ const MovieDetails = ({ movieDetails }) => {
               <button>Go Back</button>
             </section>
 
+            <section className="movie-highlights">
             <h3>{movieDetails[0].movie.title.toUpperCase()}</h3>
             <p className="tagline">{movieDetails[0].movie.tagline}</p>
             <p>Average Rating: &#11088; {movieDetails[0].movie.average_rating}</p>
-            <section className="movie-facts">
-            <p>Movie Runtime: {movieDetails[0].movie.runtime} mins</p>
-            <p>Released: {movieDetails[0].movie.release_date}</p>
-            <p>Genre: {movieDetails[0].movie.genres[0].name}</p>
-            <p>{movieDetails[0].movie.overview}</p>
-            <p>Budget: {movieDetails[0].movie.budget}</p>
-            <p>Revenue: {movieDetails[0].movie.revenue}</p>
             </section>
+
+            <section className="movie-facts">
+            <p id="i">Movie Runtime: {movieDetails[0].movie.runtime} mins</p>
+            <p id="i">Released: {movieDetails[0].movie.release_date}</p>
+            <p id="i">Genre: {movieDetails[0].movie.genres[0].name}</p>
+            <p>{movieDetails[0].movie.overview}</p>
+
+            <section className="movie-money-info">
+            <p>Budget: ${movieDetails[0].movie.budget}</p>
+            <p>Revenue: ${movieDetails[0].movie.revenue}</p>
+            </section>
+            </section>
+
           </section>
         </section>
       </section>
