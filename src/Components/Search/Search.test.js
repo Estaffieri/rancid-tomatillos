2eebad4abbe501f6bf79866c0852bddfb762ee33
Alyssa Bull <alyssabull@gmail.com>
2020-12-14 19:36:42 -0700

    Refactor search test

--- src/Components/Search/Search.test.js
+++ src/Components/Search/Search.test.js
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from '../App/App';
+import Search from './Search';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { getAllMovies } from '../../apiCalls';
@@ -8,26 +8,14 @@ jest.mock('../../apiCalls');
 
 describe('Search', () => {
   const history = createMemoryHistory();
-  let allMovies;
+  const mockInputValue = jest.fn();
 
   beforeEach(() => {
-    allMovies = {
-      movies: [
-      {
-        id: 7456,
-        poster_path:
-          "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-        backdrop_path:
-          "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-        title: "Super Fake Movie",
-        average_rating: 1.909090909090909,
-        release_date: "2020-09-04",
-        key: 7456,
-      }]
-    }
-
-    getAllMovies.mockResolvedValueOnce(allMovies);
-    render(<Router history={history}><App /></Router>)
+    render(
+      <Router history={history}>
+        <Search inputValue={mockInputValue}/>
+      </Router>
+      )
   })
 
   it('displays the user input', () => {
